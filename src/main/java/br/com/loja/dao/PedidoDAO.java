4d108e487c37d22925f392d5f0ef484edfed3751
lanyablue <barbaramy_@hotmail.com>
2022-06-29 11:59:22 -0300

    performances

--- src/main/java/br/com/loja/dao/PedidoDAO.java
+++ src/main/java/br/com/loja/dao/PedidoDAO.java
@@ -43,7 +43,11 @@ public class PedidoDAO {
 				
 	}
 	
-	
+	public Pedido buscarPedidoComCliente(Long id) {
+		return em.createQuery("SELECT p FROM Pedido p JOIN FETCH p.cliente WHERE p.id = :id", Pedido.class)
+				.setParameter("id", id)
+				.getSingleResult();
+	}
 	
 	
 	
