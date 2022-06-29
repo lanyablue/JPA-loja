efab9f4a7c8be4058de6a2a8b1759073aa868ac2
lanyablue <barbaramy_@hotmail.com>
2022-06-29 14:58:53 -0300

    embedded

---
+++ src/main/java/br/com/loja/modelo/DadosPessoais.java
@@ -0,0 +1,30 @@
+package br.com.loja.modelo;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class DadosPessoais {
+
+	private String nome;
+	private String cpf;
+	
+	public DadosPessoais() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DadosPessoais(String nome, String cpf) {
+		
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	
+}
